fd3e0b0edfda6ecb76483fe541fcdd3c23d08cf6
Terbeche Mustapha <termus96@gmail.com>
2022-05-10 09:41:47 +0100

    add projects obejevts to javascript code

--- scripts.js
+++ scripts.js
@@ -16,3 +16,48 @@ const menuLinks = document.getElementsByClassName('menu-link');
 for (let i = 0; i < menuLinks.length; i += 1) {
   menuLinks[i].addEventListener('click', hideTheMenu);
 }
+
+
+const projectsDetails = [{
+  title: 'Profesional Art Printing Data',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industry's standard",
+  technologies: ['html', 'bootstrap', 'ruby'],
+  'demo link': '#',
+  'featured image': '../images/desktop/Placeholder4.svg',
+  'source link': '#',
+},
+
+{
+  title: 'Profesional Art Printing Data',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industry's standard",
+  technologies: ['html', 'bootstrap', 'ruby'],
+  'demo link': '#',
+  'featured image': '../images/desktop/Placeholder4.svg',
+  'source link': '#',
+},
+
+{
+  title: 'Profesional Art Printing Data',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industry's standard",
+  technologies: ['html', 'bootstrap', 'ruby'],
+  'demo link': '#',
+  'featured image': '../images/desktop/Placeholder4.svg',
+  'source link': '#',
+},
+{
+  title: 'Profesional Art Printing Data',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industry's standard",
+  technologies: ['html', 'bootstrap', 'ruby'],
+  'demo link': '#',
+  'featured image': '../images/desktop/Placeholder4.svg',
+  'source link': '#',
+},
+{
+  title: 'Profesional Art Printing Data',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industry's standard",
+  technologies: ['html', 'bootstrap', 'ruby'],
+  'demo link': '#',
+  'featured image': '../images/desktop/Placeholder4.svg',
+  'source link': '#',
+},
+];
